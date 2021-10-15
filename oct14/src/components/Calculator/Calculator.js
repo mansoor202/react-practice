f2a893ab6e184be8eba817d232e67eb4495ded08
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-15 00:47:25 -0700

    added onClick,onChange events too in /ClassComponents/Calculator

--- oct14/src/components/Calculator/Calculator.js
+++ oct14/src/components/Calculator/Calculator.js
@@ -1,18 +1,21 @@
 import React from 'react'
-
+import History from './History';
 import './Calculator.css'
 
 function Calculator(){
+    const num=5;
     return(
         <>
         <h1>Simple Calculator</h1>
         <div className="input-fields">
             <input defaultValue="Enter 1st value here"/><br/><br/>
             <input defaultValue="Enter 2nd value here"/><br/><br/>
-            
-            <button className="btn1">Sum Up</button>
+
+            <button className="btn1">Sum Up {num}</button>
             
         </div>
+
+        <History/>
         </>
     )
 }
---
+++ oct14/src/components/Calculator/History.js
@@ -0,0 +1,26 @@
+import React,{Component}from "react";
+
+
+class History extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            records:[]
+        }
+    }
+
+    saveToHistory(){
+        
+
+    }
+
+    render(){
+        return (
+            <div>History saved here</div>
+        )
+    }
+}
+
+export default History
+
+
